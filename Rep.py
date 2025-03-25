339018e854e8f63fb463807818eacbd6f8cb2f81
Marcelo3901 <memc2602@gmail.com>
2025-03-25 17:41:23 -0500

    Update Rep.py

--- Rep.py
+++ Rep.py
@@ -1,25 +1,23 @@
-import streamlit as st
 import gspread
 import pandas as pd
 
-# Conexión a Google Sheets (sin credenciales)
-gc = gspread.service_account(filename=None)  # Usar None para conexión sin credenciales
+# Conectar con la hoja de Google Sheets pública sin credenciales
+gc = gspread.Client(None)
 
-# URL de tu hoja de Google Sheets
+# URL de la hoja de Google Sheets pública
 url = 'https://docs.google.com/spreadsheets/d/1FjQ8XBDwDdrlJZsNkQ6YyaygkHLhpKmfLBv6wd3uluY/edit?usp=sharing'
 
 # Abre la hoja de cálculo usando la URL
 sheet = gc.open_by_url(url)
 
 # Selecciona la hoja que deseas leer
-worksheet = sheet.get_worksheet(2)
+worksheet = sheet.get_worksheet(0)  # Primer hoja
 
 # Lee todos los registros de la hoja y conviértelos en un DataFrame de pandas
 data = worksheet.get_all_records()
 df = pd.DataFrame(data)
 
 # Muestra el DataFrame en Streamlit
+import streamlit as st
 st.title('Reporte de Inventario')
 st.write(df)
-
-# Aquí puedes agregar más lógica para procesar o filtrar la información
