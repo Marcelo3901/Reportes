afbd9d2dc3625ef7f4ba647c8cba9b1d66262894
Marcelo3901 <memc2602@gmail.com>
2025-03-25 17:46:18 -0500

    Update Rep.py

--- Rep.py
+++ Rep.py
@@ -9,7 +9,7 @@ sheet_id = '1FjQ8XBDwDdrlJZsNkQ6YyaygkHLhpKmfLBv6wd3uluY'
 range_name = 'DatosM'
 
 # Conexión con Google Sheets (acceso público)
-gc = gspread.service_account(api_key='YOUR_PUBLIC_API_KEY')
+gc = gspread.Client(auth=None)
 
 # Abrir la hoja de cálculo usando el ID de la hoja
 worksheet = gc.open_by_key(sheet_id).worksheet(range_name)
