28d526f6993d85597ea44557963699de53c5c89f
Marcelo3901 <memc2602@gmail.com>
2025-03-31 15:15:36 -0500

    Update Rep.py

--- Rep.py
+++ Rep.py
@@ -127,7 +127,7 @@ colores = {
     "Stout": "#3f3e3d",
     "Otros": "#bbb6b2",
     "Amber": "#f52615",
-    "Maracuya": "#e7e000",
+    "Maracuyá": "#e7e000",
     "Brown Ale Cafe": "#135b08"
 }
 
@@ -454,7 +454,7 @@ if not df.empty:
         "Stout": "#3f3e3d",
         "Otros": "#bbb6b2",
         "Amber": "#f52615",
-        "Maracuya": "#e7e000",
+        "Maracuyá": "#e7e000",
         "Brown Ale Cafe": "#135b08"
     }
 
